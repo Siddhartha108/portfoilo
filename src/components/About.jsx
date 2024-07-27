b703a93e63011d1a516a708ad5e8c0cd7a7d1af4
Siddhartha Sharma <6004sharma@gmail.com>
2024-07-27 23:50:24 +0530

    New Resume add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
     const pdfUrl = resume;
     const link = document.createElement("a");
     link.href = pdfUrl;
-    link.download = "document.pdf"; // specify the filename
+    // link.download = "document.pdf"; // specify the filename
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
